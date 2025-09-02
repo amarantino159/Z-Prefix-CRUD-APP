21dd692015c950f8073dd7c9f6cba75b0d8953ae
amarantino159 <amarantino159@gmail.com>
2025-09-02 09:25:48 -0700

    removed template migrates

--- api/index.js
+++ api/index.js
@@ -36,7 +36,5 @@ app.get("/item", (req, res) => {
     .join("users", "item.UserId", "=", "users.id")
     .then((data) => res.status(200).json(data))
     .catch((err) => res.status(400).json(err));
-
-
 });
 
