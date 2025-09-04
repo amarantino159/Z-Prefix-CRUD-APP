7ec2e2d90da00f1b07c46469377939c1318dd73d
amarantino159 <amarantino159@gmail.com>
2025-09-04 13:45:03 -0700

    finished Home Page

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -7,7 +7,7 @@ export function Home() {
   return (<>
   {/* basic home page to show the user the features of the site */}
     <h1>Welcome!</h1>
-    <h2>See below for the README highlights on using this Site!</h2>
+    <h2>Use the buttons to navigate between the pages</h2>
 
   </>);
 
